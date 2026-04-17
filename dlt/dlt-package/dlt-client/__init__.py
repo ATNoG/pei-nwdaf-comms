1085f08ed26bfabd94f8865e67fbef59b8806471
alxmra <miguelamneto@ua.pt>
2026-04-17 14:10:50 +0100

    package for easy usage

---
+++ dlt/dlt-package/dlt-client/__init__.py
@@ -0,0 +1,25 @@
+import os
+from .analytics import AnalyticsChannelClient
+from .inference import InferenceChannelClient
+
+__all__ = ["AnalyticsChannelClient", "InferenceChannelClient", "from_env"]
+
+
+def from_env():
+    """
+    Factory to initialize both clients from environment variables.
+
+    Environment variables:
+    - DLT_SIDECAR_URL: URL of the REST API sidecar (default: http://localhost:9337)
+    - DLT_TIMEOUT: Request timeout in seconds (default: 10.0)
+
+    Returns:
+        tuple: (AnalyticsChannelClient, InferenceChannelClient)
+    """
+    sidecar_url = os.getenv("DLT_SIDECAR_URL", "http://localhost:9337")
+    timeout = float(os.getenv("DLT_TIMEOUT", "10.0"))
+
+    return (
+        AnalyticsChannelClient(sidecar_url, timeout),
+        InferenceChannelClient(sidecar_url, timeout),
+    )
